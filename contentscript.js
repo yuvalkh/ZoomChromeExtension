513a879eb4bf08b7364c911b8a49e248630b3f83
yuvalkh <45626643+yuvalkh@users.noreply.github.com>
2020-10-27 15:39:39 +0200

    updated new url of zoom videos

--- contentscript.js
+++ contentscript.js
@@ -1,40 +1,62 @@
-/*
-  Navigate to previous page when the Backspace key is pressed.
-  Avoid this behavior when the focused element is an input field, textarea
-  or has contenteditable attribute set to true.
-
-  Not using `addEventListener` because the browser does not trigger the handler
-  when the Backspace key is pressed.
-*/
 console.debug();
-let time_to_jump = 5
+
+let time_to_jump = 5;
 let current_speed = 1;
+
 let video = document.getElementsByTagName("video")[0]; // first we get the video from the DOM
-chrome.runtime.onMessage.addListener(
-  (request, sender, sendResponse) => {
-     time_to_jump = request.jump
-     video.playbackRate = request.speed
-  });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if(request.jump == null){//which means we need to send the parameters
+    sendResponse({speed: current_speed, jump: time_to_jump})
+  }
+  else{//we got parameters
+    time_to_jump = request.jump
+    current_speed = request.speed
+    video.playbackRate = request.speed
+  }  
+});
+
 document.onkeydown = function (e) {
   //if it's not 1 of these options we just ignore it
   if (
-    document.activeElement.tagName !== "VIDEO" || // check if our focus is on the video tag(I assume there is only 1 video)
+    document.activeElement.tagName !== "VIDEO" || // check if our focus is on the video tag(we assume there is only 1 video)
     (e.key !== "ArrowRight" &&
       e.key !== "ArrowLeft" &&
       e.key !== "ArrowDown" &&
-      e.key !== "ArrowUp")
+      e.key !== "ArrowUp" && 
+      e.keyCode !== 32)// do here keycode because spacebar doesn't have a valid key
   ) {
+    console.log(document.activeElement)
     return;
   }
   e.preventDefault();
 
   if (e.key === "ArrowRight") {
     video.currentTime += time_to_jump;
+    
   } else if (e.key === "ArrowLeft") {
     video.currentTime -= time_to_jump;
   } else if (e.key === "ArrowUp") {
-    video.volume = Math.min(video.volume + 0.1, 1);
+    if(e.shiftKey) { // this is how you recognize shift key press
+      current_speed = Math.min(16,(current_speed + 0.1).toFixed(2))
+      video.playbackRate = current_speed
+    }
+    else{
+      video.volume = Math.min((video.volume + 0.1).toFixed(2), 1);
+    }
   } else if (e.key === "ArrowDown") {
-    video.volume = Math.max(video.volume - 0.1, 0);
+    if(e.shiftKey) { // this is how you recognize shift key press
+      current_speed = Math.max(0.1,(current_speed - 0.1).toFixed(2))
+      video.playbackRate = current_speed
+    }
+    else{
+      video.volume = Math.max((video.volume - 0.1).toFixed(2), 0);
+    }
+  } else if (e.keyCode === 32) {
+    if (video.paused) {
+      video.play(); 
+    }
+    else { 
+      video.pause(); 
+    } 
   }
 };
--- popup.js
+++ popup.js
@@ -6,12 +6,42 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+window.addEventListener("load", (event) => {
+    
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url; // we get the current tab and if it's zoom we let the user see the update interface
+
+        if(url.includes("zoom.us/rec")) {
+        //NEED TO SEE HOW TO GET STORAGE SPEED FROM THERE AND INITIALIZE IT    
+        chrome.storage.sync.get(['speed', 'jump'], function(items) {
+                document.getElementById("speed").value = items.speed
+                document.getElementById("jump").value = items.jump
+              });    
+            
+            document.getElementById("on_site").style.display = "block"
+            document.getElementById("not_on_site").style.display = "none"
+            getBackgroundJumpAndSpeed()
+        } else {
+            document.getElementById("on_site").style.display = "none"
+            document.getElementById("not_on_site").style.display = "block"
+        }
+    });
+    
+  });
+
 function sendToBackground(){
     //TODO: need to check if the parameters are legal
     let speed = parseFloat(document.getElementById("speed").value)
     let jump = parseFloat(document.getElementById("jump").value)
-    window.alert("after parsing: speed is " + speed + " and jump is " + jump)
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {speed: speed, jump: jump}, ()=> {})
+        chrome.tabs.sendMessage(tabs[0].id, {speed: speed, jump: jump})
+    });
+  }
+  function getBackgroundJumpAndSpeed() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {info: "give info"}, (response)=> {
+            document.getElementById("jump").value = response.jump
+            document.getElementById("speed").value = response.speed
+        })
     });
   }
